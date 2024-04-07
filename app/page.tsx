e2c7b883500f3e628fb93d1b482b4b6f85eed895
vivienzhou2014 <vivienzhou2014@gmail.com>
2024-04-06 23:25:19 -0600

    use chat and dall-e to generate a paint

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { useState } from "react";
 import { useChat } from "ai/react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Chat() {
   const { messages, append, isLoading } = useChat();
+
+  const [imageIsLoading, setImageIsLoading] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
 
   const genres = [
     {value:"Landscape"},
@@ -31,20 +36,52 @@ export default function Chat() {
     });
   };
 
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  if (imageIsLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader">
+          <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (image) {
+    return (
+      <div className="card w-full h-screen max-w-md py-24 mx-auto stretch">
+        <img src={`data:image/jpeg;base64,${image}`} />
+        <textarea
+          className="mt-4 w-full text-white bg-black h-64"
+          value={messages[messages.length - 1].content}
+          readOnly
+        />
+      </div>
+    );
+  }
+
   return (
-    <main className="mx-auto w-full p-24 flex flex-col">
-      <div className="p4 m-4">
+    <main className="mx-auto w-full p-24 flex flex-col ">
+      <div className="p4 m-4" ref={messagesContainerRef}>
         <div className="flex flex-col items-center justify-center space-y-8 text-white">
           <div className="space-y-2">
-            <h2 className="text-3xl font-bold">Story Telling App</h2>
+            <h2 className="text-3xl font-bold">Paint Generation App</h2>
             <p className="text-zinc-500 dark:text-zinc-400">
-              Customize the story by selecting the genre and tone.
+            Select a genre to generate a painting.
             </p>
           </div>
 
           {/* genre selection code */}
           <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
-            <h3 className="text-xl font-semibold">Genre</h3>
+            <h3 className="text-xl font-semibold">Themes</h3>
 
             <div className="flex flex-wrap justify-center">
               {genres.map(({ value }) => (
@@ -92,6 +129,32 @@ export default function Chat() {
           >
             {messages[messages.length - 1]?.content}
           </div>
+
+          {messages.length == 2 && !isLoading && (
+            <button
+              className="bg-blue-500 p-2 text-white rounded shadow-xl"
+              disabled={isLoading}
+              onClick={async () => {
+                setImageIsLoading(true);
+                const response = await fetch("api/images", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    message: messages[messages.length - 1].content,
+                  }),
+                });
+                const data = await response.json();
+                setImage(data);
+                setImageIsLoading(false);
+              }}
+            >
+              Generate Paint
+            </button>
+          )}
+
+
         </div>
       </div>
     </main>
